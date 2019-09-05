85f79a8653887d038b99b0b68387a3c366a46316
maxhu <jhaoheng@gmail.com>
2019-09-05 16:03:32 +0800

    callback

--- algorithm/binarySearch/main.go
+++ algorithm/binarySearch/main.go
@@ -7,7 +7,7 @@ import (
 
 var datas = []int{2, 5, 8, 12, 16, 23, 38, 56, 72, 91}
 
-var goal = 23
+var goal = 12
 
 type pointerSlice []int
 
@@ -17,28 +17,40 @@ var LoPointer, HiPointer, MidPointer int
 
 func main() {
 
-	fmt.Printf("\nSlice : %v\n\n", datas)
-
 	sliceA = datas
 	max_loop := int(math.Ceil(math.Log2(float64(len(sliceA)))))
+
+	fmt.Printf("\nSlice : %v\n", datas)
+	fmt.Printf("Search Value : %d\n", goal)
+	fmt.Printf("Search MAX Loop is :%d\n\n", max_loop)
 
 	LoPointer = 0
 	HiPointer = len(sliceA) - 1
 
 	fmt.Println("[LoIndex]-[MidIndex]-[HiIndex]")
-	for index := 0; index < max_loop; index++ {
 
-		MidPointer = (HiPointer-LoPointer)/2 + LoPointer
+	binarySearch()
 
-		fmt.Printf("% 5d % 9d % 9d\n", LoPointer, MidPointer, HiPointer)
-		if sliceA[MidPointer] > goal {
-			HiPointer = MidPointer - 1
-		} else if sliceA[MidPointer] < goal {
-			LoPointer = MidPointer + 1
-		} else {
-			fmt.Println("")
-			fmt.Println("Value :", sliceA[MidPointer], "at index:", MidPointer)
-			break
-		}
+}
+
+func binarySearch() {
+
+	if HiPointer-LoPointer < 0 {
+		fmt.Println("")
+		fmt.Println("Got Nothing.")
+		return
 	}
+
+	MidPointer = (HiPointer-LoPointer)/2 + LoPointer
+	fmt.Printf("% 5d % 9d % 9d\n", LoPointer, MidPointer, HiPointer)
+	if sliceA[MidPointer] > goal {
+		HiPointer = MidPointer - 1
+	} else if sliceA[MidPointer] < goal {
+		LoPointer = MidPointer + 1
+	} else {
+		fmt.Println("")
+		fmt.Println("Value :", sliceA[MidPointer], "at index:", MidPointer)
+		return
+	}
+	binarySearch()
 }
